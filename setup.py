bc64142e7de598d0568ad7fb313fed5658da9dbf
WAN00232 <wan00232@umn.edu>
2024-05-02 15:06:47 -0500

    correct , missingness

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         'numpy', 
         'pandas', 
-        'copy'
+        'copy',
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',  # Change as necessary
